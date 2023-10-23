f1bd434b7847e447269851953f84371d99042ddb
Akshay Yelvankar <akshayyelvankar@gmail.com>
2023-10-23 09:49:37 +0530

    new changes

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ app.use(cors(
 ))
 
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Origin', 'https://arch-design-frontend.vercel.app');
   // You can also specify which HTTP methods and headers are allowed.
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
