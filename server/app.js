4396a310245b6b85248d21df79f3d8cf632f8774
Akshay Yelvankar <akshayyelvankar@gmail.com>
2023-10-23 16:32:34 +0530

    new changes

--- server/app.js
+++ server/app.js
@@ -16,9 +16,9 @@ app.use(express.json())
 //app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle all routes by serving the index.html file
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
 app.use(cors(
   {
     origin:["https://arch-design-frontend.vercel.app"],
