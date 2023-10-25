dccd28427130364675119698391aa47df6243737
Akshay Yelvankar <akshayyelvankar@gmail.com>
2023-10-25 13:02:33 +0530

    new changes routes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <nav className="navbar navbars navbar-expand-lg navbar-light bg-light ">
                 <div className="container-fluid">
                     <NavLink className="navbar-brand nav_brands" to={'/'}>ARCH<span className="nav_text mx-2">Our Arch competetion</span></NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                 </div>
